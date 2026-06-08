3d0fb14c686025fae319679e4af7f9daac77e8cf
Demonqilin <velezloaizaesteban9012015@gmail.com>
2026-06-08 08:07:56 -0500

    Add examples for variables definitions

---
+++ chapter-1/ErrorByRedefine.hs
@@ -0,0 +1,7 @@
+module ErrorByRedefine where
+
+message = "Hello, George"
+--message = "Hiya, Porter" -- uncomment for see the error
+
+main = print message
+
---
+++ chapter-1/Variables.hs
@@ -0,0 +1,9 @@
+module Variables where
+
+salutation = "Hello"
+person = "George"
+greeting =
+  salutation <> " " <> person
+
+main = print greeting
+
